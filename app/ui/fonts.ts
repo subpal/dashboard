787ccee4e0821db4668bbc3e090315ede3237196
Subham . <subham.subham@airbnb.com>
2024-09-06 17:41:44 +0530

    Fixed font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,3 @@
 import {Inter, Lusitana} from 'next/font/google'
 export const inter = Inter({ subsets: ['latin'] });
-export const loustiana =  Lusitana({ subsets: ['latin'], weight:['400', '700'] })
+export const lustiana =  Lusitana({ subsets: ['latin'], weight:['400', '700'] })
